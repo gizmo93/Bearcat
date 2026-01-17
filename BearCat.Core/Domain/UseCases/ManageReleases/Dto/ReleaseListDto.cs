76c30d5a1bcdd1f8ea92cf996f431d80d4f65b44
gizmo_ <holmol93@gmail.com>
2026-01-17 19:54:41 +0100

    Base releases page

---
+++ BearCat.Core/Domain/UseCases/ManageReleases/Dto/ReleaseListDto.cs
@@ -0,0 +1,14 @@
+﻿using BearCat.Core.Domain.ValueObjects;
+
+namespace BearCat.Core.Domain.UseCases.ManageReleases.Dto;
+
+public record ReleaseListDto(
+    int ReleaseId,
+    string Name,
+    ReleaseType ReleaseType,
+    int ArchiveConfigCount,
+    int UploadConfigCount,
+    IReadOnlyList<ReleaseListDto.UploadConfigDto> UploadConfigStates)
+{
+    public record UploadConfigDto(string Name, OnlineState OnlineState);
+}
---
+++ BearCat.Core/Domain/UseCases/ManageReleases/Repositories/IReleaseReadRepository.cs
@@ -0,0 +1,8 @@
+﻿using BearCat.Core.Domain.UseCases.ManageReleases.Dto;
+
+namespace BearCat.Core.Domain.UseCases.ManageReleases.Repositories;
+
+public interface IReleaseReadRepository
+{
+    Task<IReadOnlyList<ReleaseListDto>> GetReleasesAsync(CancellationToken cancellationToken = default);
+}
