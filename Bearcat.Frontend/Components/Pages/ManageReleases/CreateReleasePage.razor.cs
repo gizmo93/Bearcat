8fcfa7899806626d3e2a4554efcb6fd28d9c2607
gizmo_ <holmol93@gmail.com>
2026-01-18 17:21:16 +0100

    Make path selection component more generic

--- Bearcat.Frontend/Components/Pages/ManageReleases/CreateReleasePage.razor.cs
+++ Bearcat.Frontend/Components/Pages/ManageReleases/CreateReleasePage.razor.cs
@@ -1,28 +1,32 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Bearcat.Frontend.Components.Shared;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.FluentUI.AspNetCore.Components;
 
 namespace Bearcat.Frontend.Components.Pages.ManageReleases;
 
-public partial class CreateReleasePage(IDialogService dialogService) : OwningComponentBase
+public partial class CreateReleasePage(
+    IDialogService dialogService,
+    IConfiguration configuration) : OwningComponentBase
 {
     private CreateReleaseModel model = null!;
-    
+
     private EditContext editContext = null!;
-    
+
     protected override void OnInitialized()
     {
         model = new CreateReleaseModel();
         editContext = new EditContext(model);
     }
-    
+
     private async Task ShowSelectFolderDialogAsync()
     {
-        var dialog = await dialogService.ShowDialogAsync<FolderSelectionDialog>(new DialogParameters
+        var releasesPath = configuration.GetRequiredSection("ReleaseDataDirectory").Value!;
+        var dialog = await dialogService.ShowDialogAsync<FolderSelectionDialog>(
+            data: releasesPath,
+            parameters: new DialogParameters
             {
-                Title = "Select release folder",
-                Modal = true,
-                PreventDismissOnOverlayClick = true,
+                Title = "Select release folder", Modal = true, PreventDismissOnOverlayClick = true,
             });
 
         var result = await dialog.Result;
@@ -32,4 +36,3 @@ public partial class CreateReleasePage(IDialogService dialogService) : OwningCom
         }
     }
 }
-
