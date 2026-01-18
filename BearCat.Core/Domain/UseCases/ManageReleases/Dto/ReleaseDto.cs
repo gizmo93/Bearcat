f3fb9ac84badbb2b062de2605cf1cd3b6e404316
gizmo_ <holmol93@gmail.com>
2026-01-18 18:53:32 +0100

    Base release page

---
+++ BearCat.Core/Domain/UseCases/ManageReleases/Dto/ReleaseDto.cs
@@ -0,0 +1,5 @@
+﻿using BearCat.Core.Domain.ValueObjects;
+
+namespace BearCat.Core.Domain.UseCases.ManageReleases.Dto;
+
+public record ReleaseDto(int ReleaseId, string Name, ReleaseType ReleaseType, string ReleaseFolderPath);
