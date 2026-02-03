6e008e1b3247102f8d010baaa436e12d133576d4
gizmo_ <holmol93@gmail.com>
2026-02-03 23:32:03 +0100

    Add GoFile

---
+++ Bearcat.Hosters/GoFile/Api/GetAccountId/Response.cs
@@ -0,0 +1,22 @@
+using System.Text.Json.Serialization;
+
+namespace Bearcat.Hosters.GoFile.Api.GetAccountId;
+
+public class Response
+{
+    [JsonPropertyName("status")] public string Status { get; set; } = null!;
+
+    [JsonPropertyName("data")] public Data? Data { get; set; }
+}
+
+public class Data
+{
+    [JsonPropertyName("id")] 
+    public string Id { get; set; } = null!;
+    
+    [JsonPropertyName("email")]
+    public string Email { get; set; } = null!;
+    
+    [JsonPropertyName("tier")]
+    public string Tier { get; set; } = null!;
+}
---
+++ Bearcat.Hosters/GoFile/InversionOfControl/ServiceProviderConfig.cs
@@ -0,0 +1,36 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Bearcat.Abstractions.Hoster;
+using Bearcat.Hosters.GoFile.Api;
+using Microsoft.Extensions.DependencyInjection;
+using Refit;
+
+namespace Bearcat.Hosters.GoFile.InversionOfControl;
+
+public static class ServiceProviderConfig
+{
+    extension(IServiceCollection services)
+    {
+        public void AddGoFile()
+        {
+            services.AddRefitClient<IGoFileApi>(
+                    new RefitSettings
+                    {
+                        ContentSerializer = new SystemTextJsonContentSerializer(
+                            jsonSerializerOptions: new JsonSerializerOptions
+                            {
+                                NumberHandling = JsonNumberHandling.AllowReadingFromString,
+                                PropertyNameCaseInsensitive = true,
+                            })
+                    })
+                .ConfigureHttpClient(c =>
+                {
+                    c.BaseAddress = new Uri("https://api.gofile.io");
+                    c.Timeout = Timeout.InfiniteTimeSpan;
+                });
+
+            services.AddScoped<ApiClient>();
+            services.AddKeyedScoped<IHoster, GoFile>(nameof(GoFile));
+        }
+    }
+}
