a7f070c308492d86be0fdd13417434f3b84ea0c4
gizmo_ <holmol93@gmail.com>
2026-09-15 19:50:46 +0200

    oups
    Signed-off-by: gizmo_ <holmol93@gmail.com>

---
+++ src/Bearcat.Infrastructure/Migrations/20260915174721_AddAutomaticPostingFlags.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bearcat.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAutomaticPostingFlags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EnableAutomaticPosting",
+                table: "DistributionSiteRegistrations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false
+            );
+
+            migrationBuilder.AddColumn<bool>(
+                name: "StripDotsForThreadSearch",
+                table: "DistributionSiteRegistrations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnableAutomaticPosting",
+                table: "DistributionSiteRegistrations"
+            );
+
+            migrationBuilder.DropColumn(
+                name: "StripDotsForThreadSearch",
+                table: "DistributionSiteRegistrations"
+            );
+        }
+    }
+}
